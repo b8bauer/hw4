8f6f81557c6f587af1daa150defe81c3b87f0942
b8bauer <bbauer@cca.edu>
2016-10-01 17:29:04 -0700

    Update array-practice-2.js

--- array-practice-2.js
+++ array-practice-2.js
@@ -1,26 +1,39 @@
 var length = [];
+var x; 
+var y;
+var vx; 
+var vy;
 
 function setup() {
-	
 	createCanvas (500,500);
-	background(0);
 	
 	for (var i = 0; i < height; i++){	
 		length.push(random(50,width));
 	}
 	
+	x = width/2;
+  	y = height/2;
+  	vx = 0;
+  	vy = 0;
 }
 
 function draw() {
-	var x = mouseX;
-	var y = mouseY;
-	
+	background(0);
 	for (var i = 0; i < height; i = i +2){
 		stroke(255);
 		line (0, i, length[i], i);
-		ellipse(x,y,30,30);
-
 	}
 	
+	noStroke();
+	ellipse(x-10, y-10, 30, 30);
+
+	x = x + vx;
+	y = y + vy;
+	
+	var dx = mouseX - x;
+	var dy = mouseY - y;
+	
+	vx = dx/10;
+	vy = dy/10;
 	
 }
